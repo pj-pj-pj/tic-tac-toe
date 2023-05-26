13976b3e8a678a5e86ba6ff7704de6665a1c72b9
PJ <96671928+Peaksenut@users.noreply.github.com>
2023-05-26 12:29:32 +0800

    add player x and player o that can mark board cells

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+const boardContainer = document.querySelector('gameboard');
+
 const gameBoard = (function () {
   let board = [null, null, null, null, null, null, null, null, null];
 
   for (let i = 0; i < board.length; i++) {
-    const boardContainer = document.querySelector('gameboard');
-
     const boardCell = document.createElement('div');
     boardCell.className = 'board-cell';
     boardCell.setAttribute('data-index', i);
@@ -11,3 +11,35 @@ const gameBoard = (function () {
     boardContainer.append(boardCell);
   }
 })();
+
+function createPlayer(mark) {
+  return {
+    mark: mark,
+    turn: function () {
+      this.textContent = mark;
+    },
+  };
+}
+
+const playerX = createPlayer('x');
+const playerO = createPlayer('o');
+let turnCount = 1;
+
+boardContainer.addEventListener('click', function (e) {
+  if (
+    e.target.classList.contains('board-cell') &&
+    !e.target.classList.contains('marked')
+  ) {
+    if (turnCount % 2 === 0) {
+      playerO.turn.call(e.target);
+      e.target.classList.add('o-mark');
+      e.target.classList.add('marked');
+      turnCount++;
+    } else {
+      playerX.turn.call(e.target);
+      e.target.classList.add('x-mark');
+      e.target.classList.add('marked');
+      turnCount++;
+    }
+  }
+});
