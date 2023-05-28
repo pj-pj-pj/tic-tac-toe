88e2111c3587c6b1d529929146788acfc80d3047
PJ <96671928+Peaksenut@users.noreply.github.com>
2023-05-28 12:44:18 +0800

    add vs ai(stupid)

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const boardContainer = document.querySelector('gameboard');
+const chooseMark = document.querySelector('.x-or-o');
+let turnCount = 0;
 
 const gameBoard = (function () {
   let board = [null, null, null, null, null, null, null, null, null];
@@ -10,128 +12,189 @@ const gameBoard = (function () {
       boardCell.setAttribute('data-index', i);
 
       boardContainer.append(boardCell);
+      displayTurn();
     }
   })();
 
   return {
+    get: function () {
+      return board;
+    },
     setCell: (index, marker) => {
       board[index] = marker;
     },
     checkBoard: () => {
       //check columns for winner
-      if (board[0] != null && board[0] === board[3] && board[3] === board[6]) {
-        return board[0];
-      }
-      if (board[1] != null && board[1] === board[4] && board[4] === board[7]) {
-        return board[1];
-      }
-      if (board[2] != null && board[2] === board[5] && board[5] === board[8]) {
-        return board[2];
-      }
-      //check rows for winner
-      if (board[0] != null && board[0] === board[1] && board[1] === board[2]) {
-        return board[0];
-      }
-      if (board[3] != null && board[3] === board[4] && board[4] === board[5]) {
-        return board[3];
-      }
-      if (board[6] != null && board[6] === board[7] && board[7] === board[8]) {
-        return board[6];
-      }
-      //check diagonally for winner
-      if (board[0] != null && board[0] === board[4] && board[4] === board[8]) {
-        return board[0];
-      }
-      if (board[2] != null && board[2] === board[4] && board[4] === board[6]) {
-        return board[2];
+      if (
+        (board[0] != null && board[0] === board[3] && board[3] === board[6]) ||
+        (board[1] != null && board[1] === board[4] && board[4] === board[7]) ||
+        (board[2] != null && board[2] === board[5] && board[5] === board[8]) ||
+        (board[0] != null && board[0] === board[1] && board[1] === board[2]) ||
+        (board[3] != null && board[3] === board[4] && board[4] === board[5]) ||
+        (board[6] != null && board[6] === board[7] && board[7] === board[8]) ||
+        (board[0] != null && board[0] === board[4] && board[4] === board[8]) ||
+        (board[2] != null && board[2] === board[4] && board[4] === board[6])
+      ) {
+        return player.turn ? -1 : 1;
+      } else {
+        return 0;
       }
     },
     make: function () {
       board = [null, null, null, null, null, null, null, null, null];
       boardContainer.innerHTML = '';
       turnCount = 0;
+      if (player.mark === 'x') player.turn = true;
+      if (playerAI.mark === 'x') playerAI.turn = true;
+      if (player.mark === 'o') player.turn = false;
+      if (playerAI.mark === 'o') playerAI.turn = false;
       createBoard();
     },
   };
 })();
 
-function createPlayer(mark) {
+chooseMark.showModal();
+
+function createPlayer() {
   return {
-    mark: mark,
-    turn: function () {
-      this.textContent = mark;
+    _mark: null,
+    _turn: false,
+    get mark() {
+      return this._mark;
+    },
+    set mark(value) {
+      this._mark = value;
+    },
+    get turn() {
+      return this._turn;
+    },
+    set turn(value) {
+      this._turn = value;
+    },
+    takeTurn: function (target) {
+      target.textContent = this._mark;
     },
   };
 }
 
-const playerX = createPlayer('x');
-const playerO = createPlayer('o');
-let turnCount = 0;
+const player = createPlayer();
+const playerAI = createPlayer();
 
-const root = document.querySelector('body');
-const CompStyle = getComputedStyle(root);
-const xColor = CompStyle.getPropertyValue('--accent');
-const oColor = CompStyle.getPropertyValue('--primary');
+const playasX = document.querySelector('.x');
+playasX.addEventListener('click', () => {
+  player.mark = 'x';
+  player.turn = true;
+  playerAI.mark = 'o';
+  chooseMark.close();
+  chooseMark.style.display = 'none';
 
-const currentTurn = document.querySelector('.current-turn');
+  gameBoard.make();
+});
+const playasO = document.querySelector('.o');
+playasO.addEventListener('click', () => {
+  player.mark = 'o';
+  playerAI.mark = 'x';
+  playerAI.turn = true;
+  chooseMark.close();
+  chooseMark.style.display = 'none';
 
-boardContainer.addEventListener('click', function (e) {
+  gameBoard.make();
+
+  aiTakeTurn();
+});
+
+boardContainer.addEventListener('click', (e) => {
+  const root = document.querySelector('body');
+  const CompStyle = getComputedStyle(root);
+  const borderColor = CompStyle.getPropertyValue('--accent');
+
   if (
     e.target.classList.contains('board-cell') &&
     !e.target.classList.contains('marked') &&
-    gameBoard.checkBoard() == null
+    gameBoard.checkBoard() === 0 &&
+    player.turn
   ) {
-    if (turnCount % 2 != 0) {
-      playerO.turn.call(e.target);
-      e.target.classList.add('o-mark');
-      e.target.classList.add('marked');
-      turnCount++;
-      //turn display
-      e.target.style.borderColor = xColor;
-      currentTurn.textContent = 'X';
-      currentTurn.style.fontColor = xColor;
-      // change value of board array
-      const index = e.target.getAttribute('data-index');
-      gameBoard.setCell(index, 'o');
-    } else {
-      playerX.turn.call(e.target);
-      e.target.classList.add('x-mark');
-      e.target.classList.add('marked');
-      e.target.style.borderColor = xColor;
-      turnCount++;
-      //turn display
-      e.target.style.borderColor = oColor;
-      currentTurn.textContent = 'O';
-      currentTurn.style.fontColor = oColor;
-      // change value of board array
-      const index = e.target.getAttribute('data-index');
-      gameBoard.setCell(index, 'x');
-    }
+    player.takeTurn(e.target);
+    e.target.classList.add(`${player.mark}-mark`);
+    e.target.classList.add('marked');
+    turnCount++;
+    e.target.style.borderColor = borderColor;
+    // change value of board array
+    const index = e.target.getAttribute('data-index');
+    gameBoard.setCell(index, player.mark);
+    displayTurn();
+    showResult();
+    player.turn = false;
+    playerAI.turn = true;
   }
 
-  showResult();
+  aiTakeTurn();
 });
 
-const modal = document.querySelector('dialog');
+function aiTakeTurn() {
+  setTimeout(() => {
+    if (playerAI.turn && gameBoard.checkBoard() == 0) {
+      const boardCells = document.querySelectorAll('.board-cell');
+
+      for (let i = 0; i < boardCells.length; i++) {
+        const boardCell = boardCells[i];
+        const index = boardCell.getAttribute('data-index');
+
+        if (gameBoard.get()[index] === null) {
+          turnCount++;
+          playerAI.takeTurn(boardCell); // Pass the boardCell as an argument
+          gameBoard.setCell(index, playerAI.mark);
+          boardCell.classList.add(`${player.mark}-mark`);
+          boardCell.classList.add('marked');
+          displayTurn();
+          showResult();
+          playerAI.turn = false;
+          player.turn = true;
+
+          break; // Exit the loop after making a move
+        }
+      }
+    }
+  }, 400);
+}
+
+function displayTurn() {
+  const currentTurn = document.querySelector('.current-turn');
+  const root = document.querySelector('body');
+  const CompStyle = getComputedStyle(root);
+  const borderColor = CompStyle.getPropertyValue('--accent');
+
+  if (turnCount % 2 == 0) {
+    currentTurn.textContent = 'X';
+  } else {
+    currentTurn.textContent = 'O';
+  }
+  currentTurn.style.fontColor = borderColor;
+}
+
+const modal = document.querySelector('.for-winner');
+
+const playAgainBtn = document.querySelector('.for-winner button');
+playAgainBtn.addEventListener('click', () => {
+  modal.close();
+  chooseMark.showModal();
+  chooseMark.style.display = 'grid';
+
+  aiTakeTurn();
+});
 
 function showResult() {
-  const player = gameBoard.checkBoard();
-  if (gameBoard.checkBoard() != null) {
+  if (gameBoard.checkBoard() != 0) {
     //WIN
     modal.showModal();
-    const winner = document.querySelector('dialog p');
-    winner.textContent = `${player.toUpperCase()} wins!`;
+    const winner = document.querySelector('.for-winner p');
+    if (playerAI.turn) winner.textContent = 'u lost? ಠ ʖ̯ ͡ಠ';
+    if (player.turn) winner.textContent = 'Player wins!';
   }
-  if (gameBoard.checkBoard() == null && turnCount == 9) {
+  if (gameBoard.checkBoard() == 0 && turnCount == 9) {
     //TIE
     modal.showModal();
-    const result = document.querySelector('dialog p');
+    const result = document.querySelector('.for-winner p');
     result.textContent = "It's a tie!";
   }
 }
-
-const playAgainBtn = document.querySelector('dialog button');
-playAgainBtn.addEventListener('click', () => {
-  gameBoard.make();
-  modal.close();
-});
